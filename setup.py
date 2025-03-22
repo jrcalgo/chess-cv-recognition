d34355397985011fb0fad34f747cca18adbd4847
Jackson Redman <95605366+jrcalgo@users.noreply.github.com>
2025-03-22 00:58:55 -0400

    skeleton of repo
    feel free to edit where need be, but this skeleton is approximately everything we'll need to get started

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='chess_cv_recognition',
+      version='0.0.1',
+      description='A package to recognize chess pieces from images and/or videos',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/jrcalgo/chess-cv-recognition',
+      )
