da03ee98930ea72aa18982a4eec4977d21fd8f91
Jackson Redman <95605366+jrcalgo@users.noreply.github.com>
2025-04-30 07:08:38 -0400

    stockfish edit agian

--- src/chess/stockfish_api.py
+++ src/chess/stockfish_api.py
@@ -19,16 +19,15 @@ class StockfishPlayer:
                 "UCI_Elo": stockfish_elo
             })
 
-    def get_stockfish_move(self, recent_piece_state: np.ndarray, white_time: int, black_time: int) -> Optional[tuple[tuple[int, int], tuple[int, int]]]:
+    def get_stockfish_move(self, recent_piece_state: np.ndarray) -> Optional[tuple[tuple[int, int], tuple[int, int]]]:
         best_move = None
         try:
             # Convert ChessBoardState to FEN
             fen = self._board_to_fen(recent_piece_state)
-            print()
             # Update Stockfish with the current position
             self.stockfish.set_fen_position(fen)
             # Retrieve and return the best move
-            best_move = self.stockfish.get_best_move(wtime=white_time, btime=black_time)
+            best_move = self.stockfish.get_best_move_time(5000)
             print(f"original best_move: {best_move}")
         except StockfishException as e:
             print(f"Stockfish error: {e}")
@@ -69,6 +68,3 @@ class StockfishPlayer:
             col_to = ord(best_move[2]) - ord('a')
             row_to = 8 - int(best_move[3])
             return (col_from, row_from), (col_to, row_to)
-
-    def _stockfish_api_request(self):
-
